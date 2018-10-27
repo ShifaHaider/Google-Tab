8a41120aa4b665edfea000e7ad04663af326ada2
Shifa Haider <shifa.haider065@gmail.com>
2018-10-27 10:43:33 +0500

    google-tab

--- src/app.js
+++ src/app.js
@@ -251,7 +251,6 @@ bell.setAttribute('src', 'bell-icon.png');
 Object.assign(bell.style, {
     width: '15px',
     height: '15px',
-    //top: '20px',
     //position: 'absolute',
     //right: '73px',
     //cursor: 'pointer'
